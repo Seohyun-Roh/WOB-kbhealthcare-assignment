bd2c8dd9ebd3d70162a85125c5aa43006122884a
Seohyun Roh <76952602+Seohyun-Roh@users.noreply.github.com>
2022-05-27 19:35:48 +0900

    Add: UserInfo, HealthCharts 레이아웃 (#3)
    * ADD: UserInfo 레이아웃
    
    * ADD: HealthCharts 레이아웃

--- src/assets/svgs/index.ts
+++ src/assets/svgs/index.ts
@@ -1,4 +1,5 @@
 export { ReactComponent as LogoImage } from './logo.svg'
+
 export { ReactComponent as HealthIcon1 } from './healthManage/ic-icon-mission-h-1.svg'
 export { ReactComponent as HealthIcon2 } from './healthManage/ic-icon-mission-h-2.svg'
 export { ReactComponent as HealthIcon3 } from './healthManage/ic-icon-mission-h-3.svg'
@@ -7,3 +8,6 @@ export { ReactComponent as HealthIcon5 } from './healthManage/ic-icon-mission-h-
 export { ReactComponent as HealthIcon6 } from './healthManage/ic-icon-mission-h-6.svg'
 export { ReactComponent as HealthIcon7 } from './healthManage/ic-icon-mission-h-7.svg'
 export { ReactComponent as HealthIcon8 } from './healthManage/ic-icon-mission-h-8.svg'
+
+export { ReactComponent as HealthInfo } from './healthInfo/ic-icon-health-info.svg'
+export { ReactComponent as HealthInfoArrow } from './healthInfo/ic-icon-health-info-arrow.svg'
--- src/routes/MainPage/UserInfo/index.tsx
+++ src/routes/MainPage/UserInfo/index.tsx
@@ -1,9 +1,29 @@
 import styles from './userInfo.module.scss'
+import HealthScore from './HealthScore'
+
+import { HealthInfo } from 'assets/svgs'
 
 const UserInfo = () => {
   return (
     <section className={styles.container}>
-      <h2>UserInfo</h2>
+      <h2>
+        <span>김헬스님의 건강점수</span>
+        <HealthInfo className={styles.healthInfoIcon} />
+      </h2>
+      <div className={styles.healthScoreWrapper}>
+        <HealthScore />
+        <div className={styles.infoBox}>
+          <span>기본 정보</span>
+          <dl>
+            <dt>성별</dt>
+            <dd>남성</dd>
+            <dt>나이</dt>
+            <dd>36세</dd>
+            <dt>키</dt>
+            <dd>176.8cm</dd>
+          </dl>
+        </div>
+      </div>
     </section>
   )
 }
