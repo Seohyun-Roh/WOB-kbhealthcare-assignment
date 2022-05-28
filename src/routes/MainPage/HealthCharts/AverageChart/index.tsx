6a734fb0ec727c47f7991867b654d2c34e82da0e
hyebin829 <smilerain779@gmail.com>
2022-05-28 11:23:07 +0900

    Fix : 레이아웃에 맞게 스타일 수정

--- src/routes/MainPage/HealthCharts/AverageChart/index.tsx
+++ src/routes/MainPage/HealthCharts/AverageChart/index.tsx
@@ -11,41 +11,39 @@ const AverageChart = () => {
 
   return (
     <div className={styles.container}>
-      <div className={styles.averageChartWrapper}>
-        <VictoryChart domainPadding={90}>
-          <VictoryAxis tickValues={[1, 2]} />
-          <VictoryBar
-            data={comparePeerAverage}
-            labels={({ datum }) => `${datum.HealthScore}점`}
-            x='x'
-            y='HealthScore'
-            maxDomain={{ y: 100 }}
-            minDomain={0}
-            style={{
-              data: {
-                fill: ({ datum }) => (datum.x === '나' ? '#ffd300' : '#ff9500'),
-              },
-              labels: {
-                fill: ({ datum }: CallbackArgs) => (datum.x === '나' ? '#ff9500' : 'black'),
-              },
-            }}
-          />
-          <VictoryLine data={comparePeerAverage} x='x' y='HealthScore' />
-          <VictoryScatter
-            data={comparePeerAverage}
-            x='x'
-            y='HealthScore'
-            style={{
-              data: {
-                fill: ({ datum }) => (datum.x === '나' ? 'grey' : 'white'),
-                stroke: ({ datum }) => (datum.x === '나' ? 'none' : 'black'),
-                strokeWidth: 1,
-              },
-            }}
-            size={5}
-          />
-        </VictoryChart>
-      </div>
+      <VictoryChart height={300}>
+        <VictoryAxis style={{ axis: { stroke: 'white' }, tickLabels: { fontSize: 20 } }} />
+        <VictoryBar
+          barWidth={60}
+          data={comparePeerAverage}
+          labels={({ datum }) => `${datum.HealthScore}점`}
+          x='x'
+          y='HealthScore'
+          style={{
+            data: {
+              fill: ({ datum }) => (datum.x === '나' ? '#ffd300' : '#fe612c'),
+            },
+            labels: {
+              fill: ({ datum }: CallbackArgs) => (datum.x === '나' ? '#fe612c' : 'black'),
+              fontSize: 20,
+            },
+          }}
+        />
+        <VictoryLine data={comparePeerAverage} x='x' y='HealthScore' />
+        <VictoryScatter
+          data={comparePeerAverage}
+          x='x'
+          y='HealthScore'
+          style={{
+            data: {
+              fill: ({ datum }) => (datum.x === '나' ? 'grey' : 'white'),
+              stroke: ({ datum }) => (datum.x === '나' ? 'none' : 'black'),
+              strokeWidth: 1,
+            },
+          }}
+          size={5}
+        />
+      </VictoryChart>
     </div>
   )
 }
--- src/routes/MainPage/HealthCharts/ForecastChart/index.tsx
+++ src/routes/MainPage/HealthCharts/ForecastChart/index.tsx
@@ -16,77 +16,71 @@ const ForecastChart = () => {
 
   return (
     <div className={styles.container}>
-      <h1>나의 10년 후 건강 예측</h1>
-      <div className={styles.healthScoreWrapper}>
-        <div className={styles.healthDecade}>
-          <VictoryChart domainPadding={90}>
-            <VictoryAxis tickValues={[1, 2]} />
-            <VictoryBar
-              data={compareScoreDecade}
-              x='x'
-              y='HealthScore'
-              labels={({ datum }) => `${datum.HealthScore}점`}
-              style={{
-                data: {
-                  fill: ({ datum }) => (datum.x === '나' ? '#ffd300' : '#ff9500'),
-                },
-                labels: {
-                  fill: ({ datum }: CallbackArgs) => (datum.x === '나' ? '#ff9500' : 'black'),
-                },
-              }}
-            />
-            <VictoryLine data={compareScoreDecade} x='x' y='HealthScore' />
-            <VictoryScatter
-              data={compareScoreDecade}
-              x='x'
-              y='HealthScore'
-              style={{
-                data: {
-                  fill: ({ datum }) => (datum.x === '나' ? 'grey' : 'white'),
-                  stroke: ({ datum }) => (datum.x === '나' ? 'none' : 'black'),
-                  strokeWidth: 1,
-                },
-              }}
-              size={5}
-            />
-          </VictoryChart>
-        </div>
-      </div>
-      <div className={styles.mediExpenseWrapper}>
-        <div className={styles.mediExpenseDecade}>
-          <VictoryChart domainPadding={90}>
-            <VictoryAxis tickValues={[1, 2]} />
-            <VictoryBar
-              data={compareMediExpenseDecade}
-              x='x'
-              y='mediExpense'
-              labels={({ datum }) => datum.mediExpense}
-              style={{
-                data: {
-                  fill: ({ datum }) => (datum.x === '나' ? '#ffd300' : '#ff9500'),
-                },
-                labels: {
-                  fill: ({ datum }: CallbackArgs) => (datum.x === '나' ? '#ff9500' : 'black'),
-                },
-              }}
-            />
-            <VictoryLine data={compareMediExpenseDecade} x='x' y='mediExpense' />
-            <VictoryScatter
-              data={compareMediExpenseDecade}
-              x='x'
-              y='mediExpense'
-              style={{
-                data: {
-                  fill: ({ datum }) => (datum.x === '나' ? 'grey' : 'white'),
-                  stroke: ({ datum }) => (datum.x === '나' ? 'none' : 'black'),
-                  strokeWidth: 1,
-                },
-              }}
-              size={5}
-            />
-          </VictoryChart>
-        </div>
-      </div>
+      <VictoryChart height={300}>
+        <VictoryAxis style={{ axis: { stroke: 'white' }, tickLabels: { fontSize: 20 } }} />
+        <VictoryBar
+          barWidth={60}
+          data={compareScoreDecade}
+          x='x'
+          y='HealthScore'
+          labels={({ datum }) => `${datum.HealthScore}점`}
+          style={{
+            data: {
+              fill: ({ datum }) => (datum.x === '나' ? '#ffd300' : '#fe612c'),
+            },
+            labels: {
+              fill: ({ datum }: CallbackArgs) => (datum.x === '나' ? '#fe612c' : 'black'),
+              fontSize: 20,
+            },
+          }}
+        />
+        <VictoryLine data={compareScoreDecade} x='x' y='HealthScore' />
+        <VictoryScatter
+          data={compareScoreDecade}
+          x='x'
+          y='HealthScore'
+          style={{
+            data: {
+              fill: ({ datum }) => (datum.x === '나' ? 'grey' : 'white'),
+              stroke: ({ datum }) => (datum.x === '나' ? 'none' : 'black'),
+              strokeWidth: 1,
+            },
+          }}
+          size={5}
+        />
+      </VictoryChart>
+      <VictoryChart domainPadding={60}>
+        <VictoryAxis style={{ axis: { stroke: 'white' }, tickLabels: { fontSize: 20 } }} />
+        <VictoryBar
+          data={compareMediExpenseDecade}
+          x='x'
+          y='mediExpense'
+          labels={({ datum }) => datum.mediExpense}
+          style={{
+            data: {
+              fill: ({ datum }) => (datum.x === '나' ? '#ffd300' : '#fe612c'),
+            },
+            labels: {
+              fill: ({ datum }: CallbackArgs) => (datum.x === '나' ? '#fe612c' : 'black'),
+              fontSize: 20,
+            },
+          }}
+        />
+        <VictoryLine data={compareMediExpenseDecade} x='x' y='mediExpense' />
+        <VictoryScatter
+          data={compareMediExpenseDecade}
+          x='x'
+          y='mediExpense'
+          style={{
+            data: {
+              fill: ({ datum }) => (datum.x === '나' ? 'grey' : 'white'),
+              stroke: ({ datum }) => (datum.x === '나' ? 'none' : 'black'),
+              strokeWidth: 1,
+            },
+          }}
+          size={5}
+        />
+      </VictoryChart>
     </div>
   )
 }
