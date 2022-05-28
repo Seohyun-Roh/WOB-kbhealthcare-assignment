fa9b5dca208c4ec60e66049ce11da2e9a55255f8
youngman <61752860+sksn12@users.noreply.github.com>
2022-05-29 02:13:49 +0900

    Feat: healthScoreChart UI생성 (#11)
    * Feat : healthScoreChart UI생성

--- src/routes/MainPage/UserInfo/HealthScore.tsx
+++ src/routes/MainPage/UserInfo/HealthScore.tsx
@@ -1,10 +1,36 @@
 import styles from './userInfo.module.scss'
 import { HealthInfoArrow } from 'assets/svgs'
+import { VictoryPie } from 'victory'
 
 const HealthScore = () => {
+  const percent = (875 / 1000) * 100
+  const total = 100 - percent
+
   return (
     <div className={styles.healthScoreContainer}>
-      {/* HealthScore 차트 부분. 차트 크기: 250 x 250 */}
+      <div className={styles.healthScoreChartContainer}>
+        <VictoryPie
+          data={[
+            { x: 1, y: total },
+            { x: 2, y: percent },
+          ]}
+          labels={() => null}
+          startAngle={130}
+          endAngle={-130}
+          animate={{
+            duration: 1000,
+            easing: 'bounce',
+            onLoad: { duration: 1000 },
+          }}
+          innerRadius={120}
+          colorScale={['#eeeeee', '#ffd300']}
+        />
+      </div>
+
+      <div className={styles.scoreBox}>
+        <h1 className={styles.score}>875</h1>
+        <p className={styles.scoreName}>점</p>
+      </div>
       <p className={styles.date}>2021.08.20</p>
       <p>
         건강검진결과 가져오기
